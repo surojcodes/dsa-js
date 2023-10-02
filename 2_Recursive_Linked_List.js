f0126b5b687c4096299428f5440c80413966bb69
Suroj Maharjan <surojcodes@gmail.com>
2023-10-02 15:24:19 -0500

    Recursively Reverse LL

--- 2_Recursive_Linked_List.js
+++ 2_Recursive_Linked_List.js
@@ -27,6 +27,18 @@ class LinkedList {
     this._push(value, curr.next);
   }
 
+  //reverse a linked list recursive
+  reverse(prev = null, curr = this.head) {
+    if (curr === null) {
+      this.head = prev;
+      return;
+    }
+    const next = curr.next;
+    curr.next = prev;
+    this.reverse(curr, next);
+  }
+
+  //print all values in the linked list
   traverse() {
     return this._traverse(this.head);
   }
@@ -39,4 +51,8 @@ class LinkedList {
 const ll = new LinkedList();
 ll.push(20);
 ll.push(30);
+ll.push(50);
+ll.push(70);
+console.log(ll.traverse());
+ll.reverse();
 console.log(ll.traverse());
