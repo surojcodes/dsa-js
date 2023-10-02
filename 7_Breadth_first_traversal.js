2ad43e463e701f765fc9f2bd47aa8ccd584dd407
Suroj Maharjan <surojcodes@gmail.com>
2023-10-02 16:40:02 -0500

    Implement BF Traversal

---
+++ 7_Breadth_first_traversal.js
@@ -0,0 +1,20 @@
+const tree = require('./6_Binary_Tree');
+/*
+    a
+   / \
+  b   c
+ / \   \
+d   e   f
+
+*/
+const bft = (root) => {
+  const queue = [root];
+  while (queue.length > 0) {
+    const current = queue.shift(); //shift removes from front and returns the removes item
+    console.log(current.val);
+    if (current.left !== null) queue.push(current.left); //push adds to back of queue
+    if (current.right !== null) queue.push(current.right);
+  }
+};
+
+bft(tree);
