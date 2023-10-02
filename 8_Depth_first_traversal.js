2bdf67e24577beb05926721dbf0588538230dfbf
Suroj Maharjan <surojcodes@gmail.com>
2023-10-02 16:49:41 -0500

    Implement DF Traversal

---
+++ 8_Depth_first_traversal.js
@@ -0,0 +1,20 @@
+const tree = require('./6_Binary_Tree');
+/*
+    a
+   / \
+  b   c
+ / \   \
+d   e   f
+
+*/
+const dft = (root) => {
+  const stack = [root];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current.val);
+    if (current.left !== null) stack.push(current.left);
+    if (current.right !== null) stack.push(current.right);
+  }
+};
+
+dft(tree);
