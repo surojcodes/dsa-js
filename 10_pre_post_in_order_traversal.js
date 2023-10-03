3b53143aee2c88b84f0d0305419e08eef645c24e
Suroj Maharjan <surojcodes@gmail.com>
2023-10-03 11:46:30 -0500

    Pre post in order traversal

---
+++ 10_pre_post_in_order_traversal.js
@@ -0,0 +1,38 @@
+const tree = require('./6_Binary_Tree');
+/*
+    a
+   / \
+  b   c
+ / \   \
+d   e   f
+
+*/
+
+//pre-order traversal -> parent before children (self,left,right) (abdecf)
+const DFPrintPreOrder = (root) => {
+  if (root === null) return; //tree is empty
+  console.log(root.val);
+  DFPrintPreOrder(root.left);
+  DFPrintPreOrder(root.right);
+};
+//post-order -> left, right, self (debfca)
+const DFPrintPostOrder = (root) => {
+  if (root === null) return; //tree is empty
+  DFPrintPostOrder(root.left);
+  DFPrintPostOrder(root.right);
+  console.log(root.val);
+};
+
+//in-order (left,self,right) (dbeacf)
+const DFPrintInOrder = (root) => {
+  if (root === null) return; //tree is empty
+  DFPrintInOrder(root.left);
+  console.log(root.val);
+  DFPrintInOrder(root.right);
+};
+
+DFPrintPreOrder(tree);
+console.log('--------');
+DFPrintPostOrder(tree);
+console.log('--------');
+DFPrintInOrder(tree);
