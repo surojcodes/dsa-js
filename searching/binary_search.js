9377c6ed9a6f54e7906143c9a8b96652365a50cd
Suroj Maharjan <surojcodes@gmail.com>
2023-10-09 17:46:18 -0500

    clarify computation of mid value

--- searching/binary_search.js
+++ searching/binary_search.js
@@ -16,3 +16,13 @@ console.log(binarySearch(arr, 100));
 console.log(binarySearch(arr, -9));
 console.log(binarySearch(arr, 66));
 console.log(binarySearch(arr, 10));
+
+/*
+Why does calculating mid-point have to be so complex?
+Suppose your 'l' and 'r' are 16 bit unsigned integers. 
+That means, they can only have a maximum value of 2^16=65536. 
+Consider this, l = 65530 r = 65531
+If we added them first, (l+r) would end up being junk since that big a number (131061)
+cannot be stored in a your 16-bit integer.
+And so, m  would be a wrong value.
+*/
