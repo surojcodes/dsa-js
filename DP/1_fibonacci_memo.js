2303bef0ad4d2ab0b3ed64f1ef755cb80725f20a
Suroj Maharjan <surojcodes@gmail.com>
2023-12-13 14:25:14 -0600

    DP-Memo-Fibo

---
+++ DP/1_fibonacci_memo.js
@@ -0,0 +1,12 @@
+//Fibonacci using memoization
+
+//Time : O(n)
+//Space: O(n)
+
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+};
+console.log(fib(50));
