90943857be8a66325ef9b7af3a6c9f026ce28612
Suroj Maharjan <surojcodes@gmail.com>
2023-12-18 17:42:48 -0600

    DP - fibo tabulation

---
+++ DP/tabulation/1_fibonacci.js
@@ -0,0 +1,19 @@
+/**
+ * n - input number
+ * Time: O(n)
+ * Space: O(n)
+ */
+
+const fibo = (n) => {
+  const table = new Array(n + 1).fill(0);
+  table[1] = 1;
+  for (let i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    table[i + 2] += table[i];
+  }
+  return table[n];
+};
+
+console.log(fibo(6));
+console.log(fibo(7));
+console.log(fibo(50));
