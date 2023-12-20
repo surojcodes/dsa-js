744d8216c4040cd5fc5fdc10c6fd919a879f794c
Suroj Maharjan <surojcodes@gmail.com>
2023-12-19 21:35:10 -0600

    perm

---
+++ permutation/permutation_recur.js
@@ -0,0 +1,19 @@
+//in permutation order of elements matters
+//Time Complexity: O(n!)
+//Space Complexity: O(n^2)
+
+function permutation(elements) {
+  if (elements.length === 0) return [[]];
+  const firstEl = elements[0];
+  const permsWithoutFirstEl = permutation(elements.slice(1));
+  const allPermutations = [];
+
+  permsWithoutFirstEl.forEach((perm) => {
+    for (let i = 0; i <= perm.length; i++) {
+      allPermutations.push([...perm.slice(0, i), firstEl, ...perm.slice(i)]);
+    }
+  });
+  return allPermutations;
+}
+
+console.log(permutation([1, 2, 3]));
